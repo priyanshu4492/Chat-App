ba2bd99bf2679c6042dd2ca3efeb3d8497730171
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-09-20 23:43:26 +0530

    chatApplication

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,85 @@
+import 'package:chatapp/components/my_button.dart';
+import 'package:chatapp/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  //email and password controllers
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+
+//tap to go to register page
+ final void Function()? onTap;
+
+  LoginPage({super.key,required this.onTap});
+
+  //login method
+  void login() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          const SizedBox(height: 50),
+
+          //welcome back message
+          Text(
+            "Welcome back, Dear user!",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 25),
+
+          //email textfield
+          MyTextfield(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
+          ),
+          const SizedBox(height: 10.0),
+          //pw textfield
+          MyTextfield(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwController,
+          ),
+          const SizedBox(height: 25),
+          //login button
+          MyButton(text: "Login", onTap: login),
+
+          const SizedBox(height: 25),
+          //register now
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Not a member?",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              GestureDetector(
+                onTap: onTap,
+                child: Text(
+                  "Register now",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/register_page.dart
@@ -0,0 +1,90 @@
+import 'package:chatapp/components/my_button.dart';
+import 'package:chatapp/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  final TextEditingController _confirmPwController = TextEditingController();
+
+  final void Function()? onTap;
+  RegisterPage({super.key,required this.onTap});
+
+  void register() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //logo
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+          const SizedBox(height: 50),
+
+          //welcome back message
+          Text(
+            "Let's Create an account for you",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
+          const SizedBox(height: 25),
+
+          //email textfield
+          MyTextfield(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
+          ),
+          const SizedBox(height: 10.0),
+          //pw textfield
+          MyTextfield(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwController,
+          ),
+          const SizedBox(height: 25),
+
+          //confirm password textfield
+          MyTextfield(
+            hintText: "Confirm Password",
+            obscureText: true,
+            controller: _confirmPwController,
+          ),
+          const SizedBox(height: 25),
+          //login button
+          MyButton(text: "Register", onTap: register),
+
+          const SizedBox(height: 25),
+          //register now
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Already have an account?",
+                style: TextStyle(color: Theme.of(context).colorScheme.primary),
+              ),
+              GestureDetector(
+                onTap: onTap,
+                child: Text(
+                  "Login now",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
